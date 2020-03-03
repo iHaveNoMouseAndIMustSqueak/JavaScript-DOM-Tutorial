7cc4866b09f326c465ddb2fb5644260ff421be88
Ben Newboult <ben.newboult@googlemail.com>
2020-03-03 18:37:33 +0000

    Lesson 11

--- app.js
+++ app.js
@@ -55,12 +55,21 @@ link.addEventListener('click', e => {
 */
 
 
-//Delete books
+// Delete Book
 const list = document.querySelector('#book-list ul');
- list.addEventListener('click', e => {
+
+list.addEventListener('click', e => {
    if(e.target.className === 'delete') {
      const li = e.target.parentElement;
      list.removeChild(li);
    }
  });
 
+// Add Book
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const value = addForm.querySelector('input[type="text"]').value;
+  console.log(value);
+})
