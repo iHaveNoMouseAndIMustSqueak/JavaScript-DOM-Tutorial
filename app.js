3c63620afd6fa0f43d3890441085ce2529e68a1c
Ben Newboult <ben.newboult@googlemail.com>
2020-03-03 04:23:29 +0000

    Lesson 9

--- app.js
+++ app.js
@@ -18,20 +18,37 @@ console.log(banner.hasChildNodes());
 
 const clonedBanner = banner.cloneNode(true);
 console.log(clonedBanner);
-*/
+
 
 const bookList = document.querySelector('#book-list');
 
-/*
+
 console.log(bookList.parentNode);
 console.log(bookList.parentElement.parentElement);
 
 console.log(bookList.childNodes); // Includes line breaks, is NodeList
 console.log(bookList.children); // Doesn't include line breaks, is HTMLCollection
-*/
+
 
 console.log(bookList.nextSibling); // Is a line break
 console.log(bookList.nextElementSibling);
 console.log(bookList.previousElementSibling);
 
 bookList.previousElementSibling.querySelector('p').innerHTML += '<br/>Too cool for school.'
+*/
+
+const btns = document.querySelectorAll('#book-list .delete');
+
+btns.forEach(btn => {
+  btn.addEventListener('click', e => {
+    const li = e.target.parentElement;
+    li.parentNode.removeChild(li);
+  });
+});
+
+const link = document.querySelector('#page-banner a');
+
+link.addEventListener('click', e => {
+  e.preventDefault();
+  console.log('Navigation to', e.target.textContent, 'was prevented')
+})
