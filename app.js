fe4cfbb7405009f0de5d5e022d110efb455231ce
Ben Newboult <ben.newboult@googlemail.com>
2020-03-03 19:25:50 +0000

    Lesson 16

--- app.js
+++ app.js
@@ -99,4 +99,19 @@ hideBox.addEventListener('change', e => {
   } else {
     list.style.display='initial';
   }
+});
+
+// Filter books
+const searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', e => {
+  const term = e.target.value.toLowerCase();
+  const books = list.getElementsByTagName('li');
+  Array.from(books).forEach(book => {
+    const title = book.firstElementChild.textContent;
+    if(title.toLowerCase().indexOf(term) != -1){
+      book.style.display = 'block';
+    } else {
+      book.style.display = 'none';
+    }
+  })
 })
