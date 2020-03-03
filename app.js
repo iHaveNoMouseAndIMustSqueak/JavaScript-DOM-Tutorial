48cc8db2723d80301258da5655db6973d1a8337b
Ben Newboult <ben.newboult@googlemail.com>
2020-03-03 20:00:28 +0000

    Lesson 18

--- app.js
+++ app.js
@@ -1,3 +1,81 @@
+document.addEventListener('DOMContentLoaded', () => { //For when script is at top of HTML
+  // Delete Book
+  const list = document.querySelector('#book-list ul');
+
+  list.addEventListener('click', e => {
+    if(e.target.className === 'delete') {
+      const li = e.target.parentElement;
+      list.removeChild(li);
+    }
+  });
+
+  // Add Book
+  const addForm = document.forms['add-book'];
+
+  addForm.addEventListener('submit', e => {
+    e.preventDefault(); // Prevents page reloading
+    const value = addForm.querySelector('input[type="text"]').value;
+    
+    // Create element
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    // Add content
+    deleteBtn.textContent = 'delete';
+    bookName.textContent = value;
+
+    // Add classes
+  bookName.classList.add('name');
+  deleteBtn.classList.add('delete');
+
+    // Append to document
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+  });
+
+  // Hide books
+  const hideBox = document.querySelector('#hide');
+  hideBox.addEventListener('change', e => {
+    if (hideBox.checked) {
+      list.style.display='none';
+    } else {
+      list.style.display='initial';
+    }
+  });
+
+  // Filter books
+  const searchBar = document.forms['search-books'].querySelector('input');
+  searchBar.addEventListener('keyup', e => {
+    const term = e.target.value.toLowerCase();
+    const books = list.getElementsByTagName('li');
+    Array.from(books).forEach(book => {
+      const title = book.firstElementChild.textContent;
+      if(title.toLowerCase().indexOf(term) != -1){
+        book.style.display = 'block';
+      } else {
+        book.style.display = 'none';
+      }
+    });
+  });
+
+  // Tabbed content
+  const tabs = document.querySelector('.tabs');
+  const panels = document.querySelectorAll('.panel');
+  tabs.addEventListener('click', e => {
+    if(e.target.tagName === 'LI') { // 'LI' has to be upper case to work - tagName returns an uppercase value for some reason
+      const targetPanel = document.querySelector(e.target.dataset.target);
+      panels.forEach(panel => {
+        if (panel === targetPanel) {
+          panel.classList.add('active');
+        } else {
+          panel.classList.remove('active');
+        }
+      });
+    }
+  });
+});
 /*
 let books = document.querySelectorAll('#book-list li .name'); //Selects multiple elements
 //console.log(books);
@@ -55,79 +133,3 @@ link.addEventListener('click', e => {
 */
 
 
-// Delete Book
-const list = document.querySelector('#book-list ul');
-
-list.addEventListener('click', e => {
-   if(e.target.className === 'delete') {
-     const li = e.target.parentElement;
-     list.removeChild(li);
-   }
- });
-
-// Add Book
-const addForm = document.forms['add-book'];
-
-addForm.addEventListener('submit', e => {
-  e.preventDefault(); // Prevents page reloading
-  const value = addForm.querySelector('input[type="text"]').value;
-  
-  // Create element
-  const li = document.createElement('li');
-  const bookName = document.createElement('span');
-  const deleteBtn = document.createElement('span');
-
-  // Add content
-  deleteBtn.textContent = 'delete';
-  bookName.textContent = value;
-
-  // Add classes
-bookName.classList.add('name');
-deleteBtn.classList.add('delete');
-
-  // Append to document
-  li.appendChild(bookName);
-  li.appendChild(deleteBtn);
-  list.appendChild(li);
-});
-
-// Hide books
-const hideBox = document.querySelector('#hide');
-hideBox.addEventListener('change', e => {
-  if (hideBox.checked) {
-    list.style.display='none';
-  } else {
-    list.style.display='initial';
-  }
-});
-
-// Filter books
-const searchBar = document.forms['search-books'].querySelector('input');
-searchBar.addEventListener('keyup', e => {
-  const term = e.target.value.toLowerCase();
-  const books = list.getElementsByTagName('li');
-  Array.from(books).forEach(book => {
-    const title = book.firstElementChild.textContent;
-    if(title.toLowerCase().indexOf(term) != -1){
-      book.style.display = 'block';
-    } else {
-      book.style.display = 'none';
-    }
-  });
-});
-
-// Tabbed content
-const tabs = document.querySelector('.tabs');
-const panels = document.querySelectorAll('.panel');
-tabs.addEventListener('click', e => {
-  if(e.target.tagName === 'LI') { // 'LI' has to be upper case to work - tagName returns an uppercase value for some reason
-    const targetPanel = document.querySelector(e.target.dataset.target);
-    panels.forEach(panel => {
-      if (panel === targetPanel) {
-        panel.classList.add('active');
-      } else {
-        panel.classList.remove('active');
-      }
-    })
-  }
-})
