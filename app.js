38c167e91026a4033bc51208de2271ee718cb0e3
Ben Newboult <ben.newboult@googlemail.com>
2020-03-03 03:29:24 +0000

    Lesson 4

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-console.log(wmf);
+//console.log(wmf);
 
-var books = document.querySelector('#book-list li .name');
-console.log(books);
+var books = document.querySelector('#book-list li .name'); //querySelector only selects one element
+//console.log(books);
 
-books = document.querySelectorAll('#book-list li .name');
-console.log(books);
+books = document.querySelectorAll('#book-list li .name'); //Selects multiple elements
+//console.log(books);
 
 Array.from(books).forEach(function(book){
   console.log(book);
