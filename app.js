dabb4fedfd25148645bace26645bd19f8c4a1b06
Ben Newboult <ben.newboult@googlemail.com>
2020-03-03 03:56:30 +0000

    Lesson 6

--- app.js
+++ app.js
@@ -8,3 +8,12 @@ books.forEach(function(book){
 const bookList = document.querySelector('#book-list');
 //bookList.innerHTML = '<h2>Books and books</h2>';
 //bookList.innerHTML += '<p>This is how you add HTML</p>'
+
+const banner = document.querySelector('#page-banner');
+
+console.log(banner.nodeType);
+console.log(banner.nodeName);
+console.log(banner.hasChildNodes());
+
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner);
